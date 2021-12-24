f8391c3d7935adcae6909a2217916900f709fe3e
TTMead <timothy.mead20@gmail.com>
2021-12-24 15:22:00 +1000

    Changed namespace management

--- PathFinding.cs
+++ PathFinding.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 
-namespace unityai {
+namespace MatSup {
 
     class PathFinding {
 
--- demo_files/demo_Matrix.cs
+++ demo_files/demo_Matrix.cs
@@ -1,60 +1,57 @@
 using System;
 using System.Collections;
+using MatSup;
 
-
-namespace unityai {
-
-    class Program {
-        static void Main(string[] args) {
-                
-            // Create a new 3x3 Matrix
-            Mat2D A = new Mat2D(3, 3) {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-            };
-            // Use PrintMatrix to print the value of a matrix to console
-            A.PrintMatrix();
-            // Can specify name of matrix
-            A.PrintMatrix("A");
+class Program {
+    static void Main(string[] args) {
             
+        // Create a new 3x3 Matrix
+        Mat2D A = new Mat2D(3, 3) {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        // Use PrintMatrix to print the value of a matrix to console
+        A.PrintMatrix();
+        // Can specify name of matrix
+        A.PrintMatrix("A");
+        
 
 
-            // Select a range of columns and rows from A
-            Mat2D B = A[0..1, 0..1];
-            B.PrintMatrix("B");
-
-
-
-            // Transpose a Matrix
-            (~B).PrintMatrix("Transpose of B");
-
-
-
-            // Create a Matrix of ones
-            Mat2D C = Mat2D.Ones(2, 2);
-            C.PrintMatrix("C");
-
-
-            
-            // Scalar Multiplication
-            (2*C).PrintMatrix("2C");
+        // Select a range of columns and rows from A
+        Mat2D B = A[0..1, 0..1];
+        B.PrintMatrix("B");
 
 
 
-            // Element-Wise Addition
-            Mat2D D = ~B + (2*C);
-            D.PrintMatrix("Transpose of B + 2C");
+        // Transpose a Matrix
+        (~B).PrintMatrix("Transpose of B");
 
-            // Matrix Multiplication
-            Mat2D E = ~B * (2*C);
-            E.PrintMatrix("Transpose of B multiplied by 2C");
 
-            // Element-Wise Multiplication
-            Mat2D F = ~B % (2*C);
-            F.PrintMatrix("Transpose of B element-wise muliplied by 2C");
 
-            
-        }
+        // Create a Matrix of ones
+        Mat2D C = Mat2D.Ones(2, 2);
+        C.PrintMatrix("C");
+
+
+        
+        // Scalar Multiplication
+        (2*C).PrintMatrix("2C");
+
+
+
+        // Element-Wise Addition
+        Mat2D D = ~B + (2*C);
+        D.PrintMatrix("Transpose of B + 2C");
+
+        // Matrix Multiplication
+        Mat2D E = ~B * (2*C);
+        E.PrintMatrix("Transpose of B multiplied by 2C");
+
+        // Element-Wise Multiplication
+        Mat2D F = ~B % (2*C);
+        F.PrintMatrix("Transpose of B element-wise muliplied by 2C");
+
+        
     }
 }
